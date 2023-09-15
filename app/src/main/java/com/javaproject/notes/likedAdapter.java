9d31d883ddc491d3b3e5d2a647b9309e6fe3bc20
Suraj <itsmesuru60@gmail.com>
2023-09-16 00:20:16 +0530

    * now u can view the liked notes * added animation if the liked notes is empty

---
+++ app/src/main/java/com/javaproject/notes/likedAdapter.java
@@ -0,0 +1,93 @@
+package com.javaproject.notes;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class likedAdapter extends RecyclerView.Adapter<likedAdapter.MyViewHolder> {
+    Context context;
+    static ArrayList<user_object> list;
+    int[] colors;
+    int currentColorIndex = 0;
+
+    public likedAdapter(Context context, ArrayList<user_object> list,int[] colors) {
+        this.context = context;
+        this.list = list;
+        this.colors = colors;
+    }
+
+    @NonNull
+    @Override
+    public likedAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.notes_item,parent,false);
+        return new likedAdapter.MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull likedAdapter.MyViewHolder holder, int position) {
+        user_object userObject = list.get(position);
+        holder.title.setText(userObject.getTitle());
+        holder.cont.setText(userObject.getNotescontent());
+
+
+        int currentColor = colors[currentColorIndex];
+        holder.cardView.setCardBackgroundColor(currentColor);
+
+        currentColorIndex = (currentColorIndex + 1) % colors.length;
+        holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Toast.makeText(context, "Item long pressed! "+ position, Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+
+        public CardView cardView;
+        TextView title,cont;
+
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            title = itemView.findViewById(R.id.titleFetch);
+            cont = itemView.findViewById(R.id.contentFetch);
+            cardView = itemView.findViewById(R.id.cardView);
+
+            cardView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int position = getAdapterPosition();
+                    user_object userObject = list.get(position);
+                    String id = userObject.getId();
+                    Intent intent = new Intent(itemView.getContext(), add_notes.class);
+                    intent.putExtra("id",id);
+                    intent.putExtra("clickedCardView?",1);
+                    itemView.getContext().startActivity(intent);
+                }
+            });
+
+        }
+
+    }
+}
--- app/src/main/java/com/javaproject/notes/liked_display_activity.java
+++ app/src/main/java/com/javaproject/notes/liked_display_activity.java
@@ -1,16 +1,39 @@
 package com.javaproject.notes;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 
+import com.airbnb.lottie.LottieAnimationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
 public class liked_display_activity extends AppCompatActivity implements View.OnClickListener {
 
     Button allbtn,likedBTN;
+    DatabaseReference likedREF;
+    likedAdapter likedAdapter;
+    ArrayList<user_object> list;
+    int[] colors;
+    RecyclerView recyclerView;
+    Resources resources;
+    String userID;
+    Boolean likedNoteExists = false;
+    LottieAnimationView empty;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +45,52 @@ public class liked_display_activity extends AppCompatActivity implements View.On
         window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
         window.setStatusBarColor(this.getResources().getColor(R.color.black));
         window.setNavigationBarColor(this.getResources().getColor(R.color.black));
+
+        resources = getResources();
+        colors = resources.getIntArray(R.array.card_colors);
+        userID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        recyclerView = findViewById(R.id.notesList);
+        empty = findViewById(R.id.emptyAnim);
+        empty.setVisibility(View.GONE);
+
+        likedREF = FirebaseDatabase.getInstance().getReference("notes").child(userID);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new GridLayoutManager(this,2));int spacingInPixels = getResources().getDimensionPixelSize(R.dimen.grid_spacing);
+        GridLayoutManager layoutManager = new GridLayoutManager(this, 2);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.addItemDecoration(new GridSpacingItemDecoration(2, spacingInPixels, true));
+
+        list = new ArrayList<>();
+        likedAdapter = new likedAdapter(this,list,colors);
+        recyclerView.setAdapter(likedAdapter);
+
+        likedREF.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                list.clear();
+                for(DataSnapshot dataSnapshot : snapshot.getChildren()){
+                    if (dataSnapshot.child("likedNote").getValue(Boolean.class).equals(true)){
+                        user_object userObject = dataSnapshot.getValue(user_object.class);
+                        list.add(userObject);
+                        likedNoteExists = true;
+                    }
+                }
+                if (likedNoteExists) {
+                    recyclerView.setVisibility(View.VISIBLE);
+                    likedAdapter.notifyDataSetChanged();
+                }
+                else {
+                    empty.setVisibility(View.VISIBLE);
+                    empty.setMinAndMaxProgress(0f,1f);
+                    empty.playAnimation();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
         allbtn = findViewById(R.id.allBtn);
         allbtn.setOnClickListener(this);
