78aa99e76cf20c92622cb6371f9bb2a845f54cec
Suraj <itsmesuru60@gmail.com>
2023-09-16 11:47:31 +0530

    * added swipe gestures to the screen and made navigation bar as transparent

--- app/src/main/java/com/javaproject/notes/MainActivity.java
+++ app/src/main/java/com/javaproject/notes/MainActivity.java
@@ -10,7 +10,9 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.view.GestureDetector;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -33,7 +35,7 @@ import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
 import java.util.Random;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, GestureDetector.OnGestureListener {
 
     Resources resources;
     FloatingActionButton addNote;
@@ -44,6 +46,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     Button allBTN, likedBTN;
     int[] colors;
     String userID;
+    private GestureDetector gestureDetector;
+    float x1,y1,x2,y2,x,y;
+    private static float minDistance = 150;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,8 +58,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
         window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
         window.setStatusBarColor(this.getResources().getColor(R.color.black));
-        window.setNavigationBarColor(this.getResources().getColor(R.color.black));
         setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        this.gestureDetector = new GestureDetector(MainActivity.this,this);
 
         resources = getResources();
         colors = resources.getIntArray(R.array.card_colors);
@@ -71,7 +77,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         int spacingInPixels = getResources().getDimensionPixelSize(R.dimen.grid_spacing);
         GridLayoutManager layoutManager = new GridLayoutManager(this, 2);
         recyclerView.setLayoutManager(layoutManager);
-        recyclerView.addItemDecoration(new GridSpacingItemDecoration(2, spacingInPixels, true));
+        recyclerView.addItemDecoration(new GridSpacingItemDecoration(2, 0, true));
+        recyclerView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                gestureDetector.onTouchEvent(event); // Delegate touch events to the GestureDetector
+                return false; // Return false to allow the RecyclerView to process the touch events
+            }
+        });
 
 
         list = new ArrayList<>();
@@ -114,6 +127,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             likedBTN.setTextColor(getResources().getColor(R.color.black));
             Intent intent = new Intent(MainActivity.this, liked_display_activity.class);
             startActivity(intent);
+            overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
         }
     }
 
@@ -124,5 +138,66 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         allBTN.setTextColor(getResources().getColor(R.color.black));
         likedBTN.setBackgroundColor(getResources().getColor(R.color.black));
         likedBTN.setTextColor(getResources().getColor(R.color.white));
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        gestureDetector.onTouchEvent(event);
+        switch (event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                x1 = event.getX();
+                y1 = event.getY();
+                break;
+            case MotionEvent.ACTION_UP:
+                x2 = event.getX();
+                y2 = event.getY();
+
+                x = x2-x1;
+                y = y2-y1;
+                if (Math.abs(x)>minDistance){
+                    if (x2>x1){
+                    }
+                    else {
+                        Intent intent = new Intent(this, liked_display_activity.class);
+                        startActivity(intent);
+                        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                    }
+                }
+
+        }
+
+
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    public boolean onDown(@NonNull MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public void onShowPress(@NonNull MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(@NonNull MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(@NonNull MotionEvent e1, @NonNull MotionEvent e2, float distanceX, float distanceY) {
+        return false;
+    }
+
+    @Override
+    public void onLongPress(@NonNull MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onFling(@NonNull MotionEvent e1, @NonNull MotionEvent e2, float velocityX, float velocityY) {
+        return false;
     }
 }
--- app/src/main/java/com/javaproject/notes/liked_display_activity.java
+++ app/src/main/java/com/javaproject/notes/liked_display_activity.java
@@ -5,12 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.airbnb.lottie.LottieAnimationView;
 import com.google.firebase.auth.FirebaseAuth;
@@ -22,7 +26,7 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
-public class liked_display_activity extends AppCompatActivity implements View.OnClickListener {
+public class liked_display_activity extends AppCompatActivity implements View.OnClickListener, GestureDetector.OnGestureListener {
 
     Button allbtn,likedBTN;
     DatabaseReference likedREF;
@@ -34,6 +38,9 @@ public class liked_display_activity extends AppCompatActivity implements View.On
     String userID;
     Boolean likedNoteExists = false;
     LottieAnimationView empty;
+    private GestureDetector gestureDetector;
+    float x1,y1,x2,y2,x,y;
+    private static float minDistance = 150;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,7 +51,8 @@ public class liked_display_activity extends AppCompatActivity implements View.On
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
         window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
         window.setStatusBarColor(this.getResources().getColor(R.color.black));
-        window.setNavigationBarColor(this.getResources().getColor(R.color.black));
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        this.gestureDetector = new GestureDetector(liked_display_activity.this,this);
 
         resources = getResources();
         colors = resources.getIntArray(R.array.card_colors);
@@ -58,7 +66,7 @@ public class liked_display_activity extends AppCompatActivity implements View.On
         recyclerView.setLayoutManager(new GridLayoutManager(this,2));int spacingInPixels = getResources().getDimensionPixelSize(R.dimen.grid_spacing);
         GridLayoutManager layoutManager = new GridLayoutManager(this, 2);
         recyclerView.setLayoutManager(layoutManager);
-        recyclerView.addItemDecoration(new GridSpacingItemDecoration(2, spacingInPixels, true));
+        recyclerView.addItemDecoration(new GridSpacingItemDecoration(2, 0, true));
 
         list = new ArrayList<>();
         likedAdapter = new likedAdapter(this,list,colors);
@@ -102,6 +110,74 @@ public class liked_display_activity extends AppCompatActivity implements View.On
             allbtn.setBackgroundColor(getResources().getColor(R.color.orange));
             allbtn.setTextColor(getResources().getColor(R.color.black));
             finish();
+            overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
         }
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finish();
+        overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
+    }
+
+    public boolean onTouchEvent(MotionEvent event) {
+
+        gestureDetector.onTouchEvent(event);
+        switch (event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                x1 = event.getX();
+                y1 = event.getY();
+                break;
+            case MotionEvent.ACTION_UP:
+                x2 = event.getX();
+                y2 = event.getY();
+
+                x = x2-x1;
+                y = y2-y1;
+                if (Math.abs(x)>minDistance){
+                    if (x2>x1){
+                        Intent intent = new Intent(this, MainActivity.class);
+                        startActivity(intent);
+                        overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
+                    }
+                    else {
+                    }
+                }
+
+        }
+
+
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    public boolean onDown(@NonNull MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public void onShowPress(@NonNull MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(@NonNull MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(@NonNull MotionEvent e1, @NonNull MotionEvent e2, float distanceX, float distanceY) {
+        return false;
+    }
+
+    @Override
+    public void onLongPress(@NonNull MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onFling(@NonNull MotionEvent e1, @NonNull MotionEvent e2, float velocityX, float velocityY) {
+        return false;
     }
 }
